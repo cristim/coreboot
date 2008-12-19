cristim/coreboot

commit 6d27a9207eba603f6185f029c3cb2ea4de25a428

Fix a LOT of implicit function declarations before they become errors.
Signed-off-by: Corey Osgood <corey.osgood@gmail.com>
Acked-by: Myles Watson <mylesgw@gmail.com>



git-svn-id: svn://svn.coreboot.org/coreboot/trunk@3822 2b7e53f0-3cfb-0310-b3e9-8179ed1497e1

src/northbridge/amd/amdk8/northbridge.c
@@ -16,6 +16,7 @@
 #include <string.h>
 #include <bitops.h>
 #include <cpu/cpu.h>
+#include <cpu/amd/model_fxx_rev.h>
 
 #include <cpu/x86/lapic.h>
 

src/northbridge/amd/gx1/northbridge.c
@@ -11,6 +11,7 @@
 #include "northbridge.h"
 #include <cpu/amd/gx1def.h>
 #include <cpu/x86/cache.h>
+#include <cpu/cpu.h>
 
 #define NORTHBRIDGE_FILE "northbridge.c"
 /*

src/northbridge/amd/gx2/northbridge.c
@@ -13,6 +13,7 @@
 #include <cpu/x86/msr.h>
 #include <cpu/x86/cache.h>
 #include <cpu/amd/vr.h>
+#include <cpu/cpu.h>
 #include "../../../southbridge/amd/cs5536/cs5536.h"
 #define VIDEO_MB 8
 

src/northbridge/amd/lx/northbridge.c
@@ -32,6 +32,7 @@
 #include <cpu/x86/msr.h>
 #include <cpu/x86/cache.h>
 #include <cpu/amd/vr.h>
+#include <cpu/cpu.h>
 #include "chip.h"
 #include "northbridge.h"
 #include "../../../southbridge/amd/cs5536/cs5536.h"

src/northbridge/intel/i440bx/northbridge.c
@@ -7,6 +7,8 @@
 #include <stdlib.h>
 #include <string.h>
 #include <bitops.h>
+#include <cpu/cpu.h>
+#include <pc80/keyboard.h>
 #include "chip.h"
 #include "northbridge.h"
 #include "i440bx.h"

src/northbridge/intel/i855pm/northbridge.c
@@ -3,6 +3,7 @@
 #include <stdint.h>
 #include <device/device.h>
 #include <device/pci.h>
+#include <device/pci_ids.h>
 #include <stdlib.h>
 #include <string.h>
 #include <bitops.h>
@@ -13,7 +14,6 @@
 static void pci_domain_read_resources(device_t dev)
 {
         struct resource *resource;
-        unsigned reg;
 
         /* Initialize the system wide io space constraints */
         resource = new_resource(dev, IOINDEX_SUBTRACTIVE(0, 0));

src/cpu/intel/model_f2x/model_f2x_init.c
@@ -8,6 +8,7 @@
 #include <cpu/x86/msr.h>
 #include <cpu/x86/lapic.h>
 #include <cpu/intel/microcode.h>
+#include <cpu/intel/hyperthreading.h>
 #include <cpu/x86/cache.h>
 #include <cpu/x86/mtrr.h>
 

src/include/pc80/keyboard.h
@@ -6,5 +6,6 @@ struct pc_keyboard {
 };
 
 void init_pc_keyboard(unsigned port0, unsigned port1, struct pc_keyboard *kbd);
+void set_kbc_ps2_mode(void);
 
 #endif /* PC80_KEYBOARD_H */

commit 634dd0adda0d9c46d608476d6c9a6e7d475295f9

Fix breakage caused by r3822. I should have known not to touch the k8 stuff...
Signed-off-by: Corey Osgood <corey.osgood@gmail.com>
Acked-by: Corey Osgood <corey.osgood@gmail.com>



git-svn-id: svn://svn.coreboot.org/coreboot/trunk@3824 2b7e53f0-3cfb-0310-b3e9-8179ed1497e1

src/northbridge/amd/amdk8/northbridge.c
@@ -16,7 +16,6 @@
 #include <string.h>
 #include <bitops.h>
 #include <cpu/cpu.h>
-#include <cpu/amd/model_fxx_rev.h>
 
 #include <cpu/x86/lapic.h>
 