cristim/coreboot

commit 6d27a9207eba603f6185f029c3cb2ea4de25a428

Fix a LOT of implicit function declarations before they become errors.
Signed-off-by: Corey Osgood <corey.osgood@gmail.com>
Acked-by: Myles Watson <mylesgw@gmail.com>



git-svn-id: svn://svn.coreboot.org/coreboot/trunk@3822 2b7e53f0-3cfb-0310-b3e9-8179ed1497e1

src/mainboard/dell/s1850/reset.c
@@ -1,4 +1,5 @@
 #include <arch/io.h>
+#include <device/pci.h>
 #include <device/pci_def.h>
 #include <device/pci_ids.h>
 #ifndef __ROMCC__

src/cpu/intel/model_f2x/model_f2x_init.c
@@ -8,6 +8,7 @@
 #include <cpu/x86/msr.h>
 #include <cpu/x86/lapic.h>
 #include <cpu/intel/microcode.h>
+#include <cpu/intel/hyperthreading.h>
 #include <cpu/x86/cache.h>
 #include <cpu/x86/mtrr.h>
 

src/include/pc80/keyboard.h
@@ -6,5 +6,6 @@ struct pc_keyboard {
 };
 
 void init_pc_keyboard(unsigned port0, unsigned port1, struct pc_keyboard *kbd);
+void set_kbc_ps2_mode(void);
 
 #endif /* PC80_KEYBOARD_H */

src/mainboard/amd/pistachio/fadt.c
@@ -26,6 +26,7 @@
 #include <arch/acpi.h>
 #include <arch/io.h>
 
+extern void pm_iowrite(u8 reg, u8 value);
 /*extern*/ u16 pm_base = 0x800;
 /* pm_base should be set in sb acpi */
 /* pm_base should be got from bar2 of rs690. Here I compact ACPI

src/mainboard/amd/pistachio/mainboard.c
@@ -37,6 +37,9 @@ extern void pm2_iowrite(u8 reg, u8 value);
 extern int do_smbus_read_byte(u32 smbus_io_base, u32 device, u32 address);
 extern int do_smbus_write_byte(u32 smbus_io_base, u32 device, u32 address,
 			       u8 val);
+extern void lb_add_memory_range(struct lb_memory *mem, uint32_t type, 
+				uint64_t start, uint64_t size);
+
 #define ADT7475_read_byte(address) \
 	 do_smbus_read_byte(SMBUS_IO_BASE, ADT7475_ADDRESS, address)
 #define ADT7475_write_byte(address, val) \

src/northbridge/amd/amdk8/northbridge.c
@@ -16,6 +16,7 @@
 #include <string.h>
 #include <bitops.h>
 #include <cpu/cpu.h>
+#include <cpu/amd/model_fxx_rev.h>
 
 #include <cpu/x86/lapic.h>
 

src/northbridge/amd/gx1/northbridge.c
@@ -11,6 +11,7 @@
 #include "northbridge.h"
 #include <cpu/amd/gx1def.h>
 #include <cpu/x86/cache.h>
+#include <cpu/cpu.h>
 
 #define NORTHBRIDGE_FILE "northbridge.c"
 /*

src/northbridge/amd/gx2/chipsetinit.c
@@ -14,7 +14,7 @@
 #include <cpu/x86/cache.h>
 #include "../../../southbridge/amd/cs5536/cs5536.h"
 
-
+extern int sizeram(void);
 /* the structs in this file only set msr.lo. But ... that may not always be true */
 
 struct msrinit {

commit 1a141c84758af9dd78a6d2fc7eccd8a1b12b4f74

Fix dell/s1850 broken in r3822, and prepare it for implicit declaration error patch.
Signed-off-by: Corey Osgood <corey.osgood@gmail.com>
Acked-by: Corey Osgood <corey.osgood@gmail.com>



git-svn-id: svn://svn.coreboot.org/coreboot/trunk@3828 2b7e53f0-3cfb-0310-b3e9-8179ed1497e1

src/mainboard/dell/s1850/reset.c
@@ -1,9 +1,9 @@
 #include <arch/io.h>
-#include <device/pci.h>
+
 #include <device/pci_def.h>
 #include <device/pci_ids.h>
 #ifndef __ROMCC__
-#include <device/device.h>
+#include <device/pci.h>
 #define PCI_ID(VENDOR_ID, DEVICE_ID) \
 	((((DEVICE_ID) & 0xFFFF) << 16) | ((VENDOR_ID) & 0xFFFF))
 #define PCI_DEV_INVALID 0
@@ -12,6 +12,8 @@ static inline device_t pci_locate_device(unsigned pci_id, device_t from)
 {
 	return dev_find_device(pci_id >> 16, pci_id & 0xffff, from);
 }
+#else
+#include <arch/romcc_io.h>
 #endif
 
 void soft_reset(void)